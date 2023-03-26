849a90da532615d162e5aa94c2ba382c9e4542e7
srinivasnemani01 <srinivas.nemani@live.com>
2023-03-26 22:54:39 +0000

    fastapi_streamlit
    fastapi_streamlit

--- src/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-This package provides test cases for the B76OptionPricer and OptionPricer classes, which are part of 
- b76_model and api.option_pricer modules respectively. 
- 
- It validates the correctness of the calculate_option_prices and 
- calculate_market_prices methods for these classes. Mock data is provided through the 
- MockDataPersistence_Call and MockDataPersistence_Put classes for testing purpose.
-
-"""
